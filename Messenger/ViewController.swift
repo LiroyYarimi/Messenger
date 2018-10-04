e57e4c0665a6ebb98f30fcaf494abf46f5b70fcd
liroy y <liroy.yarimi@gmail.com>
2018-10-04 10:05:35 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by liroy yarimi on 04/10/2018.
+//  Copyright © 2018 Liroy Yarimi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
